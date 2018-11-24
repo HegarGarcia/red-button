517c91d9b7bae1a95573777c6fadceeeb1e8f3ab
Hegar Garcia <hegargarcia@gmail.com>
2018-11-24 08:47:48 -0600

    Configuring firebase for web platform

--- web_patform/src/environments/environment.prod.ts
+++ web_patform/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBcSX4s4GKhotJcA90mF-EMQeN1A-k4_Cc',
+    authDomain: 'security-colima-2018.firebaseapp.com',
+    databaseURL: 'https://security-colima-2018.firebaseio.com',
+    projectId: 'security-colima-2018',
+    storageBucket: 'security-colima-2018.appspot.com',
+    messagingSenderId: '296041100489'
+  }
 };
--- web_patform/src/environments/environment.ts
+++ web_patform/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBcSX4s4GKhotJcA90mF-EMQeN1A-k4_Cc',
+    authDomain: 'security-colima-2018.firebaseapp.com',
+    databaseURL: 'https://security-colima-2018.firebaseio.com',
+    projectId: 'security-colima-2018',
+    storageBucket: 'security-colima-2018.appspot.com',
+    messagingSenderId: '296041100489'
+  }
 };
 
 /*

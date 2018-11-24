b205cdd3c63e6ffa7e4d78c0cbfdde0272a0b0c3
Oliver Rios <olivermaximiliano2@gmail.com>
2018-11-24 11:23:06 -0600

    Adding Ionic Google Maps

--- native_app/src/app/home/home.page.ts
+++ native_app/src/app/home/home.page.ts
@@ -1,3 +1,13 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +15,46 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+  map: GoogleMap;
+  constructor() { }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyAsmY7Ka-rUSyGbwsooh0XTJlBLLckRt_o',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyAsmY7Ka-rUSyGbwsooh0XTJlBLLckRt_o'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+}

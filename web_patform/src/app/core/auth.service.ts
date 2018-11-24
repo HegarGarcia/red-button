1ddf346a43d431430b8e38b1109b27c8422a3d08
Hegar Garcia <hegargarcia@gmail.com>
2018-11-24 15:36:41 -0600

    Fixing firebase/auth import

--- web_patform/src/app/core/auth.service.ts
+++ web_patform/src/app/core/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/auth';
+import { auth } from 'firebase';
 
 @Injectable()
 export class AuthService {

d4a15d4bda9a86779156b1c6fd60d8b06a4360d8
Hegar Garcia <hegargarcia@gmail.com>
2018-11-24 12:43:14 -0600

    Fixing problem in service declaration

--- native_app/src/app/core/auth.service.ts
+++ native_app/src/app/core/auth.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
   authState: firebase.User;
 

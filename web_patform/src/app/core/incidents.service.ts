69f178555fa3afee39c6e33563f9a8bfe2d10075
Hegar Garcia <hegargarcia@gmail.com>
2018-11-24 12:43:41 -0600

    Adding core services and module

---
+++ web_patform/src/app/core/incidents.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+import { IIncidentPayload } from './core.module';
+
+@Injectable()
+export class IncidentsService {
+  colectionRef: Observable<any>;
+
+  constructor(private firestore: AngularFirestore) {}
+
+  addIncident(payload: IIncidentPayload) {
+    return this.firestore.collection('incidents').add(payload);
+  }
+
+  getIncidents() {
+    return this.firestore.collection('incidents').get();
+  }
+}

75539d4a08e3dc5b4c33088805e0e692c92b9691
UlisesMR <38411593+UlisesMR@users.noreply.github.com>
2018-11-24 19:04:27 -0600

    botoncito perron

--- web_patform/src/app/app.module.ts
+++ web_patform/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MapViewModule } from './map-view/map-view.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CoreModule } from '@core/core.module';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AgmCoreModule } from '@agm/core';
 
@@ -25,6 +26,7 @@ import { AgmCoreModule } from '@agm/core';
     AppRoutingModule,
     MatSidenavModule,
     MatButtonModule,
+    MatIconModule,
     CoreModule,
     AgmCoreModule,
     AgmCoreModule.forRoot({

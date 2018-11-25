30408ada4e938f8c7f9ede0ff61d371280036114
Oliver Rios <olivermaximiliano2@gmail.com>
2018-11-24 20:15:50 -0600

    Adding media

--- native_app/src/app/home/home.page.ts
+++ native_app/src/app/home/home.page.ts
@@ -73,7 +73,7 @@ export class HomePage implements OnInit {
         longitude: this.location.latLng.lng
       },
       datatime: +(Date.now()),
-      event: 'atropellado'
+      event: 'atropello'
     };
 
     this.incident.addIncident(payload);
